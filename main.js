4780b76e207a2d5896afbfa9cea3dcca5d40dbf6
aSanchidrian <sanchimon.alex@gmail.com>
2022-11-10 20:50:48 +0100

    cambios varios

---
+++ main.js
@@ -0,0 +1,23 @@
+function clickDiv()
+{
+    window.location.href = '/index.html';
+    return false;
+}
+
+$(document).ready(function()
+{
+    $("#buscador").click(function()
+    {
+        console.log(test);
+    });
+
+    $("#login").click(function()
+    {
+        
+    });
+
+    $("#signup").click(function()
+    {
+        
+    });
+})
