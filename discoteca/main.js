eb70bee895dcc1c4832d58c7d52bd7e59991b3ea
pauidiaz <94858816+pauidiaz@users.noreply.github.com>
2022-12-01 14:38:01 +0100

    Add files via upload
    plantilla de las discotecas indivudualmente

---
+++ discoteca/main.js
@@ -0,0 +1,25 @@
+function clickDiv()
+{
+    window.location.href = '..\Proyectos3-main\discoteca';
+    return false;
+}
+
+$(document).ready(function()
+{
+    $("#buscador").click(function()
+    {
+        console.log(test);
+    });
+
+    $("#login").click(function()
+    {
+        
+    });
+
+    $("#signup").click(function()
+    {
+        
+    });
+	
+})
+
