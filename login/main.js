86bbdc421030e5255d1371e43b5b7ac27c6e017c
aSanchidrian <sanchimon.alex@gmail.com>
2022-12-16 11:25:13 +0100

    linkados y update de login

---
+++ login/main.js
@@ -0,0 +1,28 @@
+var userName;
+var userSurname;
+var userEmail;
+var userPass;
+
+function clickDiv()
+{
+    window.location.href = '../Inicio/index.html';
+    return false;
+}
+
+$(document).ready(function()
+{
+    $("#buscador").click(function()
+    {
+        console.log(test);
+    });
+
+    $("#login").click(function()
+    {
+        
+    });
+
+    $("#log").click(function()
+    {
+        
+    });
+})
